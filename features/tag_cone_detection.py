c7b1a155626547987d801b0327d1d4899d2d3494
sxy-sun <sunxycn98@gmail.com>
2024-10-31 19:05:24 -0400

    added yolo-tag-detection

--- features/tag_cone_detection.py
+++ features/tag_cone_detection.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env python3
 from flask import Flask, Response
 import numpy as np
+from ultralytics import YOLO
 
 from utils.utils import register_signal_handlers
 from utils.config import CAMERA_CONFIG
 from camera.camera import Camera
+from camera.cone_detector import ConeDetector
 from camera.apriltag_detector import AprilTagDetector
-
 """
 Features:
-1. Displays the video live stream with apriltag detection to the browser.
+1. Displays the video live stream with tag and cone detection to the browser.
 2. Display the pose estimate values.
 
 visit: http://your_mbot_ip:5001
 """
 
-class AprilTagViewer(Camera):
-    def __init__(self, camera_id, width, height, calibration_data, fps=None):
+class TagConeViewer(Camera):
+    def __init__(self, camera_id, width, height, model, calibration_data, fps=None):
         super().__init__(camera_id, width, height, fps)
+        self.cone_detector = ConeDetector(model, calibration_data)
         self.tag_detector = AprilTagDetector(calibration_data)
 
     def process_frame(self, frame):
+        self.cone_detector.detect_cones(frame)
+        self.cone_detector.draw_cone_detect(frame)
         self.tag_detector.detect_tags(frame)
         self.tag_detector.draw_tags(frame)
 
@@ -41,7 +45,16 @@ if __name__ == '__main__':
 
     calibration_data = np.load('cam_calibration_data.npz')
 
-    camera = AprilTagViewer(camera_id, image_width, image_height,
-                                      calibration_data, fps)
+    # Load a YOLO11n PyTorch model
+    # model = YOLO("../utils/cone_detection_model.pt")
+
+    # Export the model to NCNN format
+    # model.export(format="ncnn")
+
+    # Load the exported NCNN model
+    ncnn_model = YOLO("../utils/cone_detection_model_ncnn_model")
+
+    camera = TagConeViewer(camera_id, image_width, image_height,
+                                    ncnn_model, calibration_data, fps)
     register_signal_handlers(camera.cleanup)
     app.run(host='0.0.0.0', port=5001)
